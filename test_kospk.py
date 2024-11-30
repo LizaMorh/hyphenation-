80e30042d786971b2b2306bc24f072b95f44b7e3
LizaMorh <25102006liza@gmail.com>
2024-11-30 10:34:59 +0200

    Update test_kospk.py

--- test_kospk.py
+++ test_kospk.py
@@ -4,7 +4,7 @@ from kospk import transfer_word
 
 def test_transfer_word():
     assert transfer_word("наприклад") == "на-при-клад"
-    assert transfer_word("український") == "укра-їн-сь-кий"
+    assert transfer_word("український") == "укра-їнсь-кий"
     assert transfer_word("розроблення") == "роз-роб-лен-ня"
     assert transfer_word("програми") == "про-гра-ми"
     assert transfer_word("це") == "це"
@@ -12,6 +12,6 @@ def test_transfer_word():
     assert transfer_word("лис") == "лис"
     assert transfer_word("кота") == "ко-та"
     assert transfer_word("тест") == "тест"
-    assert transfer_word("один") == "о-дин"
+    assert transfer_word("один") == "один"
     assert transfer_word("привіт") == "при-віт"
     assert transfer_word("маленький") == "ма-лень-кий"  
